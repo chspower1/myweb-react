f7a04d737ce409bb064b0b55605dd9b554c9c402
chspower1 <chspower1@gmail.com>
2022-09-27 00:52:41 +0900

    setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import NavBar from "./components/NavBar";
+import Router from "./Router";
 function App() {
-    return <></>;
+    return (
+        <>
+            <NavBar />
+            <Router />
+        </>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createGlobalStyle } from "styled-components";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { lightTheme } from "./theme";
+import reset from "styled-reset";
+import "./fonts/fonts.css";
 
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const GlobalStyled = createGlobalStyle`
+  ${reset}
+  body{
+    font-size:16px;
+    font-family: "Sebang";
+    box-sizing: border-box;
+    text-decoration: none;
+  }
+  h1{
+        font-family:"SebangBold";
+    }
+`;
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <ThemeProvider theme={lightTheme}>
+            <GlobalStyled />
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>
 );
